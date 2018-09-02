1c168981a12b33d2e2bc05e795879bf8c2ed05e8
Aixi Wang <aixi.wang@hotmail.com>
2018-09-02 22:00:14 +0800

    Add files via upload
    Initial checkin. The right of sketchflat.exe and gcode_viewer.exe belongs to their own owner.

---
+++ autoexec.g
@@ -0,0 +1,7 @@
+G92 X0 Y0 Z0
+G01 X0 Y0 Z0 F120
+G90
+G21
+m21
+m23 auto.nc
+m24
---
+++ combine.bat
@@ -0,0 +1,10 @@
+del *.bak
+type start.nc > auto.nc
+type 1.nc >> auto.nc
+type 2.nc >> auto.nc
+type 3.nc >> auto.nc
+type 4.nc >> auto.nc
+type 5.nc >> auto.nc
+type stop.nc >> auto.nc
+dir *.nc
+pause
